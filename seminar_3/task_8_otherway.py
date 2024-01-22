bd03170427fc62cb6f299fdc736184efb5d77b58
Riley-study <120293475+Riley-study@users.noreply.github.com>
2024-01-22 10:35:57 +0500

    seminar 4 start

---
+++ seminar_3/task_8_otherway.py
@@ -0,0 +1,52 @@
+# Три друга взяли вещи в поход. Сформируйте словарь,
+# где ключ - имя друга, а значение - кортеж вещей. Ответьте на вопросы:
+
+# какие вещи взяли все три друга
+
+# какие вещи уникальны, есть только у одного друга
+
+# какие вещи есть у всех друзей кроме одного и имя того,
+# у кого данная вещь отсутствует
+
+# Для решения используйте операции с множествами.
+# Код должен расширяться на любое большее количество друзей.
+
+hike = {
+    'Aaz': ("спички", "спальник", "дрова", "топор"),
+    'Skeeve': ("спальник", "спички", "вода", "еда"),
+    'Tananda': ("вода", "спички", "косметичка"),
+}
+
+temp_bag = None
+for things in hike.values():
+    if not temp_bag:
+        temp_bag = set(things)
+        continue
+    temp_bag = temp_bag.intersection(set(things))
+print(temp_bag)
+
+result_uniq = {}
+for name, things in hike.items():
+    temp = set(things)
+    temp_o = set()
+    for other in hike.values():
+        if things == other:
+            continue
+        temp_o = temp_o.union(set(other))
+    temp = temp.difference(temp_o)
+    if temp:
+        result_uniq.update({name: temp})
+print(result_uniq)
+
+res_missing = {}
+for name, things in hike.items():
+    temp = set(things)
+    for other in hike.values():
+        if things == other:
+            continue
+        temp_o = temp_o.intersection(set(other))
+    temp = temp_o.difference(temp)
+    if temp:
+        res_missing.update({name: temp})
+
+print(res_missing)
