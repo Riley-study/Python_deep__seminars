931e7d4feda3e69d8b8e2fbf25af89a107cd3777
Riley-study <120293475+Riley-study@users.noreply.github.com>
2024-01-22 12:49:44 +0500

    seminar 4 task 6

--- seminar_4/task_6.py
+++ seminar_4/task_6.py
@@ -4,7 +4,9 @@
 # Если нижняя граница меньше нуля, суммируем от начала.
 # Если верхняя граница больше длины списка, до конца.
 
-def sum_numbers_between_two_index(in_list: list[int | float], start_index: int, end_index: int) -> int | float:
+def sum_numbers_between_two_index(in_list: list[int | float],
+                                  start_index: int,
+                                  end_index: int) -> int | float:
     """ returns sum of numbers"""
     if start_index > end_index:
         start_index, end_index = end_index, start_index
@@ -17,4 +19,4 @@ def sum_numbers_between_two_index(in_list: list[int | float], start_index: int, 
 
 list_init = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 45, 67, 8, 99]
 
-print(sum_numbers_between_two_index(list_init, -6, 25))
+print(sum_numbers_between_two_index(list_init, 10, 0))
