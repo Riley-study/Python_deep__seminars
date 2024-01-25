9e8f819390e68d683c0e1e26b0f0ba49940d0b3a
Riley-study <120293475+Riley-study@users.noreply.github.com>
2024-01-25 12:15:30 +0500

    seminar 5 tasks 1-3

---
+++ seminar_5/task_3.py
@@ -0,0 +1,6 @@
+# Создайте генератор чётных чисел от нуля до 100. Из последовательности исключите числа,
+# сумма цифр которых равна 8. Решение в одну строку.
+
+gen_even_numbers = (number for number in range(0, 101, 2) if number % 8 != 0)
+for i in gen_even_numbers:
+    print(i)
